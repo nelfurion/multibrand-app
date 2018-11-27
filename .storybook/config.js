e203f4f38a95c5746642dd94d2d7f4f1342ae253
nelfurion <angelovradostin@gmail.com>
2018-11-27 01:29:43 +0000

    adding theme and decorator

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,12 +3,87 @@ import VueInfoAddon from 'storybook-addon-vue-info'
 
 import Vue from 'vue';
 
+import { themes } from '@storybook/components';
+
+import { withOptions } from '@storybook/addon-options';
+
 const req = require.context('../src/components', true, /stories\.js$/);
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
 
-addDecorator(VueInfoAddon)
+addDecorator(
+  withOptions({
+    theme: themes.dark,
+    name: 'Zava',
+    url: '#',
+    /**
+     * show story component as full screen
+     * @type {Boolean}
+     */
+    goFullScreen: false,
+    /**
+     * display panel that shows a list of stories
+     * @type {Boolean}
+     */
+    showStoriesPanel: true,
+    /**
+     * display panel that shows addon configurations
+     * @type {Boolean}
+     */
+    showAddonPanel: true,
+    /**
+     * display floating search box to search through stories
+     * @type {Boolean}
+     */
+    showSearchBox: true,
+    /**
+     * show addon panel as a vertical panel on the right
+     * @type {Boolean}
+     */
+    addonPanelInRight: false,
+    /**
+     * sorts stories
+     * @type {Boolean}
+     */
+    sortStoriesByKind: false,
+    /**
+     * regex for finding the hierarchy separator
+     * @example:
+     *   null - turn off hierarchy
+     *   /\// - split by `/`
+     *   /\./ - split by `.`
+     *   /\/|\./ - split by `/` or `.`
+     * @type {Regex}
+     */
+    hierarchySeparator: null,
+    /**
+     * regex for finding the hierarchy root separator
+     * @example:
+     *   null - turn off multiple hierarchy roots
+     *   /\|/ - split by `|`
+     * @type {Regex}
+     */
+    hierarchyRootSeparator: null,
+    /**
+     * sidebar tree animations
+     * @type {Boolean}
+     */
+    sidebarAnimations: true,
+    /**
+     * id to select an addon panel
+     * @type {String}
+     */
+    selectedAddonPanel: undefined, // The order of addons in the "Addon panel" is the same as you import them in 'addons.js'. The first panel will be opened by default as you run Storybook
+    /**
+     * enable/disable shortcuts
+     * @type {Boolean}
+     */
+    enableShortcuts: true, // true by default
+  })
+);
+
+addDecorator(VueInfoAddon);
 
 configure(loadStories, module);
--- src/components/BrandedButton/stories.js
+++ src/components/BrandedButton/stories.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 
 import { storiesOf } from '@storybook/vue'
-
+import { addDecorator } from '@storybook/vue'
 import BrandedButton from './BrandedButton.vue'
+
+process.env.asd = '@/components/BrandedButton'
+import '@/stories-styles/brand1.scss'
+
 
 Vue.component('BrandedButton', BrandedButton)
 
@@ -10,6 +14,10 @@ const storyConfig = (template) => ({
   componets: { BrandedButton },
   template
 })
+
+addDecorator(() => ({
+  template: '<div style="textAlign: center"><story/></div>'
+}))
 
 storiesOf('BrandedButton', module)
   .add('BrandedButton primary', () => storyConfig('<BrandedButton type="primary" />'))
